d8af97c6fcc75c1043da7071d3841bb333d47a30
Luděk Veselý <ludek.vesely@email.com>
2015-12-09 17:12:18 +0100

    typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,10 +13,10 @@ if [ "$1" = 'kibana' ]; then
 		: ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
 		sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
 		sed -i \
-      -e "s#-KIBANA_INDEX-#${KIBANA_INDEX}#" \
-      -e "s#-KIBANA_ELASTICSEARCH_USERNAME-#${KIBANA_ELASTICSEARCH_USERNAME}#" \
-      -e "s#-KIBANA_ELASTICSEARCH_PASSWORD-#${KIBANA_ELASTICSEARCH_PASSWORD}#" \
-      /opt/kibana/config/kibana.yml
+			-e "s#-KIBANA_INDEX-#${KIBANA_INDEX}#" \
+			-e "s#-KIBANA_ELASTICSEARCH_USERNAME-#${KIBANA_ELASTICSEARCH_USERNAME}#" \
+			-e "s#-KIBANA_ELASTICSEARCH_PASSWORD-#${KIBANA_ELASTICSEARCH_PASSWORD}#" \
+			/opt/kibana/config/kibana.yml
 	else
 		echo >&2 'warning: missing ELASTICSEARCH_PORT_9200_TCP or ELASTICSEARCH_URL'
 		echo >&2 '  Did you forget to --link some-elasticsearch:elasticsearch'
